0685ed4fc952d01f7bdcb9c40bc69a5420e81f31
Naveen Gujar <naveengujar@live.com>
2021-02-21 15:04:58 +0100

    corrections for tests

--- aquasec/resource_enforcer_group_test.go
+++ aquasec/resource_enforcer_group_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestAquasecenforcerGroup(t *testing.T) {
-	group_id := "created_by_terraform"
+	group_id := "terraform"
 	description := "Created"
 	logical_name := "terraform-eg"
 	enforce := false
@@ -26,7 +26,7 @@ func TestAquasecenforcerGroup(t *testing.T) {
 			{
 				Config: testAccCheckEnforcerGroup(group_id, description, logical_name, enforce, gateways, etype, otype, service_account, namespace, master),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckEnforcerGroupExists("aquasec_enforcer_groups.terraform-eg"),
+					testAccCheckEnforcerGroupExists("aquasec_enforcer_groups.terraformeg"),
 				),
 			},
 		},
@@ -35,7 +35,7 @@ func TestAquasecenforcerGroup(t *testing.T) {
 
 func testAccCheckEnforcerGroup(group_id string, description string, logical_name string, enforce bool, gateways string, etype string, otype string, service_account string, namespace string, master bool) string {
 	return fmt.Sprintf(`
-	resource "aquasec_enforcer_groups" "terraform-eg" {
+	resource "aquasec_enforcer_groups" "terraformeg" {
 		group_id = "%s"
 		description = "%s"
 		logical_name = "%s"
