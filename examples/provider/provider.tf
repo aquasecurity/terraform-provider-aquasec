7eae80e59b6b5625bf14731d7d122c8d0248dda3
Emerson Hardisty <emerson.hardisty@ecs.co.uk>
2021-11-24 10:10:50 +0000

    Align comments and fix spacing error

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 provider "aquasec" {
-  username = "IaC"  // Alternatively sourced from $AQUA_USER
+username = "IaC"                    // Alternatively sourced from $AQUA_USER
   aqua_url = "https://aquaurl.com"  // Alternatively sourced from $AQUA_URL
-  password = "@password"  // Alternatively sourced from $AQUA_PASSWORD
+  password = "@password"            // Alternatively sourced from $AQUA_PASSWORD
 
   // If you are using unverifiable certificates (e.g. self-signed) you may need to disable certificate verification
-  verify_tls = false // Alternatively sourced from $AQUA_TLS_VERIFY
+  verify_tls = false                // Alternatively sourced from $AQUA_TLS_VERIFY
 
   // Alternatively, you can provide these configurations from a config file, and configure the provider as below
   // config_path = '/path/to/tf.config' // defaults to '~/.aqua/tf.config' -- Alternatively sourced from $AQUA_CONFIG
