e150488b7c4844144facf9cbdc9d0b4efe8aeb2e
yossigilad-aquasec <yossi.gilad@aquasec.com>
2022-07-04 13:56:32 +0300

    Removing permission set verification before sending api creation, it will move the verification to aqua api. Updating resource example for permissions_sets and enforcer_groups

--- examples/resources/aquasec_permissions_sets/resource.tf
+++ examples/resources/aquasec_permissions_sets/resource.tf
@@ -1,38 +1,98 @@
 resource "aquasec_permissions_sets" "my_terraform_perm_set" {
-    name = "my_terraform_perm_set"
-    description     = "created from terraform"
-    author    = "system"
-    ui_access = true
-    is_super = false
+        name        = "my_terraform_perm_set"
+    description = "Test Permissions Sets created by Terraform"
+    author      = "system"
+    ui_access   = true
+    is_super    = false
     actions = [
-        "dashboard.read",
-        "risks.vulnerabilities.read",
-        "risks.vulnerabilities.write",
-        "risks.host_images.read",
-        "risks.benchmark.read",
-        "risk_explorer.read",
-        "images.read",
-        "image_profiles.read",
-        "image_assurance.read",
-        "image_assurance.write",
-        "runtime_policies.read",
-        "runtime_policies.write",
-        "functions.read",
-        "gateways.read",
-        "secrets.read",
-        "audits.read",
-        "containers.read",
-        "enforcers.read",
-        "infrastructure.read",
-        "consoles.read",
-        "settings.read",
-        "network_policies.read",
-        "acl_policies.read",
-        "acl_policies.write",
-        "services.read",
-        "integrations.read",
-        "registries_integrations.read",
-        "web_hook.read",
-        "incidents.read"
+        #################
+    # Policies
+    #################
+    # Assurance Policies
+    "acl_policies.read",                # Removed from version 2022.4
+    "acl_policies.write",               # Removed from version 2022.4
+    # Image Profiles
+    "image_profiles.read",
+    "image_profiles.write",             # Only for version 2022.4
+    # Firewall Policies
+    "network_policies.read",
+    "network_policies.write",           # Only for version 2022.4
+    # Runtime Policies
+    "runtime_policies.read",
+    "runtime_policies.write",
+    # Response Policies                 # Only for version 2022.4
+    "response_policies.read",           # Only for version 2022.4
+    "response_policies.write",          # Only for version 2022.4
+    # User Access Control Policies
+    "image_assurance.read",
+    "image_assurance.write",
+
+    #################
+    # Assets
+    #################
+    # Dashboard
+    "dashboard.read",
+    "dashboard.write",                  # Only for version 2022.4
+    # Risk Explorer
+    "risk_explorer.read",
+    # Images
+    "images.read",
+    "images.write",                     # Only for version 2022.4
+    # Host Images
+    "risks.host_images.read",
+    "risks.host_images.write",          # Only for version 2022.4
+    # Functions
+    "functions.read",
+    "functions.write",                  # Only for version 2022.4
+    # Enforcers
+    "enforcers.read",
+    "enforcers.write",                  # Only for version 2022.4
+    # Containers
+    "containers.read",
+    # Services
+    "services.read",
+    "services.write",                   # Only for version 2022.4
+    # Infrastructure
+    "infrastructure.read",
+    "infrastructure.write",             # Only for version 2022.4
+
+    #################
+    # Compliance
+    #################
+    # Vulnerabilities
+    "risks.vulnerabilities.read",
+    "risks.vulnerabilities.write",
+    # CIS Benchmarks
+    "risks.benchmark.read",
+    "risks.benchmark.write",            # Only for version 2022.4
+
+    #################
+    # System
+    #################
+    # Audit Events
+    "audits.read",
+    # Secrets
+    "secrets.read",
+    "secrets.write",                    # Only for version 2022.4
+    # Settings
+    "settings.read",
+    "settings.write",                   # Only for version 2022.4
+    # Integrations
+    "integrations.read",
+    "integrations.write",               # Only for version 2022.4
+    # Image Registry Integrations
+    "registries_integrations.read",
+    "registries_integrations.write",    # Only for version 2022.4
+    # Scanner CLI                       # Only for version 2022.4
+    "scan.read",                        # Only for version 2022.4
+    # Gateways
+    "gateways.read",
+    "gateways.write",                   # Only for version 2022.4
+    # Consoles
+    "consoles.read",
+    # Webhook authorization API
+    "web_hook.read",
+    # Incidents
+    "incidents.read"
     ]
 }
