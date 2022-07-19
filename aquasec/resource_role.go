364512f3b08338a20e0e9c20f9ca40f6c937b763
byteford <sandford.james@gmail.com>
2022-07-19 14:18:47 +0100

    feat: add import to role

--- aquasec/resource_role.go
+++ aquasec/resource_role.go
@@ -1,10 +1,11 @@
 package aquasec
 
 import (
-	"github.com/aquasecurity/terraform-provider-aquasec/client"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"log"
 	"time"
+
+	"github.com/aquasecurity/terraform-provider-aquasec/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceRole() *schema.Resource {
@@ -17,39 +18,39 @@ func resourceRole() *schema.Resource {
 		Update: resourceRoleUpdate,
 		Delete: resourceRoleDelete,
 		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
+			StateContext: schema.ImportStatePassthroughContext,
 		},
 		Schema: map[string]*schema.Schema{
 			"role_name": {
-				Type:     schema.TypeString,
+				Type:        schema.TypeString,
 				Description: "The name of the role, comprised of alphanumeric characters and '-', '_', ' ', ':', '.', '@', '!', '^'.",
-				Required: true,
+				Required:    true,
 			},
 			"author": {
-				Type:     schema.TypeString,
+				Type:        schema.TypeString,
 				Description: "The name of the user who created the role. Only returned from the API for existing permissions, not part of the permission creation/modification structure.",
-				Computed: true,
+				Computed:    true,
 			},
 			"description": {
-				Type:     schema.TypeString,
+				Type:        schema.TypeString,
 				Description: "Free text description for the role.",
-				Optional: true,
+				Optional:    true,
 			},
 			"updated_at": {
-				Type:     schema.TypeString,
+				Type:        schema.TypeString,
 				Description: "The date of the last modification of the role.",
-				Computed: true,
-				Optional: true,
+				Computed:    true,
+				Optional:    true,
 			},
 			"permission": {
-				Type:     schema.TypeString,
+				Type:        schema.TypeString,
 				Description: "The name of the Permission Set that will affect the users assigned to this specific Role.",
-				Required: true,
+				Required:    true,
 			},
 			"scopes": {
-				Type:     schema.TypeList,
+				Type:        schema.TypeList,
 				Description: "List of Application Scopes that will affect the users assigned to this specific Role.",
-				Required: true,
+				Required:    true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
@@ -66,22 +67,17 @@ func resourceRoleCreate(d *schema.ResourceData, m interface{}) error {
 	if err != nil {
 		return err
 	}
+	d.SetId(role.Name)
+	return resourceRoleRead(d, m)
 
-	err = resourceRoleRead(d, m)
-	if err == nil {
-		d.SetId(role.Name)
-	} else {
-		return err
-	}
-	return nil
 }
 
 func resourceRoleRead(d *schema.ResourceData, m interface{}) error {
 	ac := m.(*client.Client)
 
-	id := d.Get("role_name").(string)
-	r, err := ac.GetRole(id)
+	r, err := ac.GetRole(d.Id())
 	if err == nil {
+		d.Set("name", r.Name)
 		d.Set("updated_at", r.UpdatedAt)
 		d.Set("author", r.Author)
 	} else {
