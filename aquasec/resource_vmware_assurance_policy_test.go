6192dd0a83ea7c67a319f186f115a43c14fc88e5
Baruch Bilanski <baruch.bilanski@aquasec.com>
2023-12-27 14:35:04 +0200

    vmware tests added

---
+++ aquasec/resource_vmware_assurance_policy_test.go
@@ -0,0 +1,63 @@
+package aquasec
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAquasecVMwareAssurancePolicy(t *testing.T) {
+	t.Parallel()
+	description := "Created using Terraform"
+	name := acctest.RandomWithPrefix("terraform-test")
+	application_scopes := "Global"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: CheckDestroy("aquasec_vmware_assurance_policy.terraformiap"),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckVMwareAssurancePolicy(description, name, application_scopes),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckVMwareAssurancePolicyExists("aquasec_vmware_assurance_policy.terraformiap"),
+				),
+			},
+			{
+				ResourceName:      "aquasec_vmware_assurance_policy.terraformiap",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckVMwareAssurancePolicy(description string, name string, application_scopes string) string {
+	return fmt.Sprintf(`
+	resource "aquasec_vmware_assurance_policy" "terraformiap" {
+		description = "%s"
+		name = "%s"
+		application_scopes = [
+			"%s"
+		]
+	}`, description, name, application_scopes)
+
+}
+
+func testAccCheckVMwareAssurancePolicyExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return NewNotFoundErrorf("%s in state", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return NewNotFoundErrorf("ID for %s in state", n)
+		}
+
+		return nil
+	}
+}
