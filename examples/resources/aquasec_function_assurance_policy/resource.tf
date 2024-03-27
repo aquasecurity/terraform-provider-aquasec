45accc572159b50d7bb34f28836bc3a57c82a2b0
Richard Julian <richard@rjulian.net>
2024-03-27 16:41:38 +0100

    Add in example for function assurance policy

---
+++ examples/resources/aquasec_function_assurance_policy/resource.tf
@@ -0,0 +1,17 @@
+resource "aquasec_function_assurance_policy" "example_function_assurance_policy" {
+  //Required values
+  application_scopes = ["Global"]
+  name               = "example_function_assurance_policy"
+
+  //Values that default to true
+  audit_on_failure = true
+  block_failed     = true
+  fail_cicd        = true
+
+  function_integrity_enabled    = true
+  enforce_excessive_permissions = true
+  scan_sensitive_data           = true
+  cvss_severity                 = "critical"
+  cvss_severity_enabled         = true
+
+}
