3af82e670aa302ff647f3a15cab3ab4b2f7a8e1c
Sarvodaya Kumar <sarvodaya.kumar@outlook.com>
2025-07-24 11:13:08 +0530

    fix: TP | Fix aquasec_registry (resource and datasource)
    Implementation:
    * Updated the schema with adding the missing attribute
      which API was receiving but was not mentioned in the schema.
    * Updated the Terraform acceptance test to increase the coverage
      with providing different scanner_type.
    * Updated the document and example with adding all the newly
      added attribute.

--- examples/resources/aquasec_integration_registry/resource.tf
+++ examples/resources/aquasec_integration_registry/resource.tf
@@ -1,17 +1,21 @@
 resource "aquasec_integration_registry" "integration_registry" {
-  name                          = "integration_registry"
-  type                          = "AWS"
-  advanced_settings_cleanup     = false
-  always_pull_patterns          = [":latest", ":v1"]
-  author                        = "aqua@aquasec.com"
-  auto_cleanup                  = false
-  auto_pull                     = true
-  auto_pull_interval            = 1
-  auto_pull_max                 = 100
-  auto_pull_rescan              = false
-  auto_pull_time                = "08:45"
-  description                   = "Automatically discovered registry"
-  image_creation_date_condition = "image_count"
+  name                           = "integration_registry"
+  type                           = "AWS"
+  advanced_settings_cleanup      = false
+  always_pull_patterns           = [":latest", ":v1"]
+  author                         = "aqua@aquasec.com"
+  auto_cleanup                   = false
+  auto_pull                      = true
+  auto_pull_interval             = 1
+  auto_pull_max                  = 100
+  auto_pull_rescan               = false
+  auto_pull_time                 = "08:45"
+  description                    = "Automatically discovered registry"
+  image_creation_date_condition  = "image_count"
+  permission                     = "GlobalPermission"
+  nexus_mtts_ff_enabled          = true
+  auto_pull_latest_xff_enabled   = true
+  is_architecture_system_default = false
 
   options {
     option = "ARNRole"
@@ -34,14 +38,19 @@ resource "aquasec_integration_registry" "integration_registry" {
   pull_image_count            = 3
   pull_image_tag_pattern      = [":Latest", ":latest"]
   pull_repo_patterns_excluded = [":xyz", ":onlytest"]
+  pull_repo_patterns          = [""]
+  pull_tags_pattern           = [""]
+  pull_max_tags               = 1
 
   url          = "us-east-1"
   scanner_name = []
   scanner_type = "any"
   scanner_group_name = "terraform-test"  //configure when scanner_type is "specific"
 
-  username = ""
-  password = ""
+  username    = ""
+  password    = ""
+  client_cert = ""
+  client_key  = ""
   webhook {
     enabled       = true
     url           = "https://aquasec.com/"
@@ -55,5 +64,7 @@ resource "aquasec_integration_registry" "integration_registry" {
     time            = "2025-07-09T08:45:00Z" //YYYY-MM-DDTHH:MM:SSZ
     week_days       = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"] // ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   }
-}
 
+  force_ootb = false
+  force_save = false
+}
